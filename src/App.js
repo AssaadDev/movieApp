b77913858fd6c541c068b9cba1db8efd30462ca8
Sharker <assaadahmed7@gmail.com>
2022-02-01 20:41:33 +0100

    Having problem with git and github

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React,{useState} from "react";
+
+function App() {
+
+    const [srch, setSrch] = useState('');
+    //console.log(value);
+
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/movies">Movies</Link>
+          <Link to="/series">Series</Link>
+          <input type={"text"} placeholder="Search..." value={srch} onChange={e => setSrch(e.target.value)}/>
+        </header>
+        <Routes>
+          <Route path="/movies" />
+          <Route exat path="/movies/${id}" />
+          <Route exat path="/series" />
+          <Route exat path="/series/${id}" />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
